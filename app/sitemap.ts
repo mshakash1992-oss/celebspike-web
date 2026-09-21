e037b8df2db9180885b957906de4559737695fae
Showkot Hossain <msh.akash1992@gmail.com>
2026-09-21 22:16:11 +0300

    Make sitemap update dynamically from Sanity

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,11 @@
 import {MetadataRoute} from 'next'
 import {client} from '@/lib/sanity'
 
+// Sanity-তে নতুন post publish/update হওয়ার পর
+// sitemap যেন পুরোনো cached version না দেখায়।
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 type SitemapPost = {
   slug: {
     current: string
@@ -12,13 +17,22 @@ type SitemapPost = {
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://www.celebspike.com'
 
-  const posts: SitemapPost[] = await client.fetch(`
-    *[_type == "post" && defined(slug.current)] | order(publishedAt desc) {
-      slug,
-      publishedAt,
-      _updatedAt
-    }
-  `)
+  const posts: SitemapPost[] = await client.fetch(
+    `
+      *[
+        _type == "post" &&
+        defined(slug.current)
+      ] | order(publishedAt desc) {
+        slug,
+        publishedAt,
+        _updatedAt
+      }
+    `,
+    {},
+    {
+      cache: 'no-store',
+    },
+  )
 
   const postUrls: MetadataRoute.Sitemap = posts.map((post) => ({
     url: `${baseUrl}/blog/${post.slug.current}`,
